f9782f01aef00ee091d4c6ef1785520f987ed62f
cesarsicas <7783112+cesarsicas@users.noreply.github.com>
2024-12-11 18:26:18 -0300

    wip already calling service using load balance client

--- src/main/java/br/com/cesarsicas/springstore/domain/user_auth/AuthController.java
+++ src/main/java/br/com/cesarsicas/springstore/domain/user_auth/AuthController.java
@@ -9,12 +9,16 @@ import br.com.cesarsicas.springstore.domain.user_auth.dto.LoginDto;
 import br.com.cesarsicas.springstore.domain.user_auth.dto.RegisterDto;
 import br.com.cesarsicas.springstore.domain.user_auth.dto.TokenJWTDto;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/auth")
@@ -28,6 +32,12 @@ public class AuthController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    RestTemplate restTemplate;
 
 
     @PostMapping("/login")
@@ -46,6 +56,22 @@ public class AuthController {
     public ResponseEntity register(@RequestBody @Valid RegisterDto registerDto) {
         userService.saveUser(new User(registerDto));
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/teste")
+    @Transactional
+    public ResponseEntity teste() {
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("SPRINGSTORE-PAYMENT-SERVICE");
+
+        String uri = serviceInstance.getUri().toString();
+
+        System.out.println("Uri >>>>> " + uri);
+
+        var result = restTemplate.getForObject(uri + "/payments/api/home", String.class);
+
+
+        return ResponseEntity.ok("Chegou aqui e retornou : "+result);
     }
 
 }
